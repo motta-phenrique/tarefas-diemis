c69a9de0e7ce183462a55e47657a1dccab5a879d
Paulo Henrique Ribeiro Motta <128097269+motta-phenrique@users.noreply.github.com>
2024-05-16 22:07:35 -0300

    Add files via upload

---
+++ trabalho-dieimesd/script.js
@@ -0,0 +1,7 @@
+
+const imagens = document.querySelectorAll('img');
+console.log(imagens);
+
+
+const paragrafoTexto = "Esperamos que esses destinos de viagem tenham despertado sua curiosidade e inspirado sua próxima aventura!";
+document.body.innerHTML += `<p>${paragrafoTexto}</p>`;
